a33995462f4a9a4e380e03f8eb15431d919ecbc9
kushal-10 <kushalkoshti11@gmail.com>
2025-04-23 22:59:45 +0200

    add retriever with score filter

--- src/rag/retriever.py
+++ src/rag/retriever.py
@@ -2,12 +2,18 @@ import os
 import json
 import weaviate
 from tqdm import tqdm
+import logging
 from typing import Dict
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_weaviate.vectorstores import WeaviateVectorStore
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename=os.path.join("src", "rag", "retreiver.log"),
+    level=logging.INFO
+    )
 
 class Retrieve():
 
@@ -15,7 +21,7 @@ class Retrieve():
     Extract Relevant passages for each sub-target for a particular PDF
     """
 
-    def __init__(self, embeddings = OpenAIEmbeddings(), weaviate_client = None, targets_path: str = os.path.join("data", "targets.json")):
+    def __init__(self, embeddings = OpenAIEmbeddings(), weaviate_client = None, targets_path: str = os.path.join("results", "targets.json")):
         """
         Args:
             embeddings: Embedding model to generate Weaviate docs, defaults to OpenAI Embeddings
@@ -48,27 +54,48 @@ class Retrieve():
 
         return db
     
-    # @staticmethod
-    # def retriever_query():
-    #     pass
-
-    # @staticmethod
-    # def classification_query():
-    #     pass
     
-    def retrieve_docs(self, db: WeaviateVectorStore = None) -> Dict:
+    def retrieve_docs(self, db: WeaviateVectorStore = None, similarity_threshold: float = 0.75) -> Dict:
         """
-        Return a Dict object containing T/F Classifications of each Sub-Target for a given annual report
+        Return a Dict object containing extracted passages of each Sub-Target for a given annual report
 
         Args:
             db: Weaviate Database based on sepearate documents from a given text file 
         
         Returns:
-            classifications: A dict containing classifications of each Sub-Target
+            retrieved_docs: A dict containing relevant passages of each Sub-Target
         """
 
-        query = "Does the company have a policy to improve employee health & safety?"
-        docs = db.similarity_search_with_score(query, k=5)
+        logger.info(f"Retrieving Docs with threshold : {similarity_threshold}")
+        retrieved_docs = []
+        for i in range(1, 18):
+            # Iterate over each Goal
+            sub_target_def = self.targets[str(i)]
+            for defn in sub_target_def:
+                retrieved_count = 0
+                docs = db.similarity_search_with_score(defn, k=50) # Retrieve top 50 relevant docs
+                # Filter by similarity score
+                for doc in docs:
+                    if doc[1] >= similarity_threshold: # Check doc score
+                        retrieved_count += 1
+                        retrieved_docs.append(
+                            {
+                                "goal": i,
+                                "sub_target": defn,
+                                "page_content": doc[0].page_content
+                            }
+                        )
+            
+                if not retrieved_count:
+                    logger.info(f"No Retrieved docs for \nGoal - {i}, \nSub-Target - {defn}\n")
+                    logger.info("*"*100)
+                else:
+                    logger.info(f"Retrieved {retrieved_count} docs for Goal - {i}, \nSub-Target - {defn}\n")
+                    logger.info("*"*100)
+
+
+        return retrieved_docs
+                    
 
 
 if __name__ == '__main__':
@@ -77,15 +104,13 @@ if __name__ == '__main__':
     weaviate_client = weaviate.connect_to_local()
     openai_embeddings = OpenAIEmbeddings()
     
-    classifier = Retrieve(embeddings=openai_embeddings, weaviate_client=weaviate_client, targets_path=os.path.join("data", "targets.json"))
+    doc_retriever = Retrieve(embeddings=openai_embeddings, weaviate_client=weaviate_client, targets_path=os.path.join("results", "targets.json"))
     txt_pth = os.path.join("data", "txts", "14.BASF_$42.93 B_Industrials", "2023", "results.txt")
-    db = classifier.get_db(txt_pth)
+    db = doc_retriever.get_db(txt_pth)
+
+    retrieved_docs = doc_retriever.retrieve_docs(db=db, similarity_threshold=0.75)
 
     # Close client connection
     weaviate_client.close()
-
-
-# for doc in docs:
-#     print(f"{doc[1]:.3f}", ":", doc[0].page_content)
 
 
