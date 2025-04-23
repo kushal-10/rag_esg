bb8564fc052a7fc77e598fd4ba91ccab1207941d
kushal-10 <kushalkoshti11@gmail.com>
2025-04-23 22:22:28 +0200

    base struct for Classify

--- src/rag/hybrid_search.py
+++ src/rag/hybrid_search.py
@@ -1,23 +1,92 @@
+import os
+import json
+import weaviate
+from tqdm import tqdm
+from typing import Dict
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-import weaviate
 from langchain_weaviate.vectorstores import WeaviateVectorStore
 
-loader = TextLoader("data/txts/14.BASF_$42.93 B_Industrials/2023/results.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
+class Classify():
 
-weaviate_client = weaviate.connect_to_local()
-db = WeaviateVectorStore.from_documents(docs, embeddings, client=weaviate_client)
+    """
+    Get binary classifications for a particular sub-target for a particular PDF
+    """
 
-query = "By 2030, ensure sustainable food production systems and implement resilient agricultural practices that increase productivity and production, that help maintain ecosystems, that strengthen capacity for adaptation to climate change, extreme weather, drought, flooding and other disasters and that progressively improve land and soil quality"
-docs = db.similarity_search_with_score("country", k=50)
+    def __init__(self, embeddings = OpenAIEmbeddings(), weaviate_client = None, targets_path: str = os.path.join("data", "targets.json")):
+        """
+        Args:
+            embeddings: Embedding model to generate Weaviate docs, defaults to OpenAI Embeddings
+            weaviate_client: Local docker image for Weaviate
+            targets_path: Path to a JSON file containing definitions of sub-targets
+        """
+        self.embeddings = embeddings
+        self.weaviate_client = weaviate_client
+        with open(targets_path, 'r') as f:
+            self.targets = json.load(f)
 
-for doc in docs:
-    print(f"{doc[1]:.3f}", ":", doc[0].page_content[:100] + "...")
 
-weaviate_client.close()
+    def get_db(self, file_path: str, chunk_size: int = 1000, chunk_overlap: int = 0) -> WeaviateVectorStore:
+        """
+        Return a Weaviate Database by splitting the given text file
+        Args:
+            file_path: The path of the text file of a given annual report
+            chunk_size: The size of chunks in characters to form a document
+            chunk_overlap: Overlap of characters between consecutive documents
+
+        Returns:
+            db: Weaviate Database based on sepearate documents from the text file
+        """
+
+        loader = TextLoader(file_path)
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        docs = text_splitter.split_documents(documents)
+        db = WeaviateVectorStore.from_documents(docs, self.embeddings, client=self.weaviate_client)
+
+        return db
+    
+    @staticmethod
+    def retriever_query():
+        pass
+
+    @staticmethod
+    def classification_query():
+        pass
+    
+    def get_classifications(self, db: WeaviateVectorStore = None) -> Dict:
+        """
+        Return a Dict object containing T/F Classifications of each Sub-Target for a given annual report
+
+        Args:
+            db: Weaviate Database based on sepearate documents from a given text file 
+        
+        Returns:
+            classifications: A dict containing classifications of each Sub-Target
+        """
+
+        query = "Does the company have a policy to improve employee health & safety?"
+        docs = db.similarity_search_with_score(query, k=5)
+
+
+if __name__ == '__main__':
+
+    # Initialize Weaviate Client locally via Docker and Initialize OpenAI Embeddings
+    weaviate_client = weaviate.connect_to_local()
+    openai_embeddings = OpenAIEmbeddings()
+    
+    classifier = Classify(embeddings=openai_embeddings, weaviate_client=weaviate_client, targets_path=os.path.join("data", "targets.json"))
+    txt_pth = os.path.join("data", "txts", "14.BASF_$42.93 B_Industrials", "2023", "results.txt")
+    db = classifier.get_db(txt_pth)
+    classifications = classifier.get_classifications(db)
+
+    # Close client connection
+    weaviate_client.close()
+
+
+# for doc in docs:
+#     print(f"{doc[1]:.3f}", ":", doc[0].page_content)
+
+
